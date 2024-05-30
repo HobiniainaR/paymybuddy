17baa9cbadf412cc83a6a2dc2d220b950199f01f
Hoby <rabhob@yahoo.fr>
2024-05-30 10:40:37 +0200

    Initialisation du projet paymybuddy

---
+++ src/test/java/com/hobiniaina/paymybuddy/PaymybuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobiniaina.paymybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymybuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
