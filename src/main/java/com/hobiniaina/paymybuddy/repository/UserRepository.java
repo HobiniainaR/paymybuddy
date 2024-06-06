cfda991746d592f9bbda36b0bda6ae1bbe4dd1b7
Hoby <rabhob@yahoo.fr>
2024-06-06 13:04:09 +0200

    corrections du Pom et application properties avec le Mentor

--- src/main/java/com/hobiniaina/paymybuddy/repository/UserRepository.java
+++ src/main/java/com/hobiniaina/paymybuddy/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.hobiniaina.paymybuddy.repository;
 
-import org.apache.catalina.User;
+
+import com.hobiniaina.paymybuddy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
